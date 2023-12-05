8b6121dffede2c42960b03df70ac65a0c075395f
Joshua Krusell <joshua.krusell@gu.se>
2023-12-05 16:08:26 +0200

    Run for full sample

--- hmm.R
+++ hmm.R
@@ -3,50 +3,41 @@
 library(cmdstanr)
 library(dplyr)
 
-options(mc.cores = 4)
+options(mc.cores = parallel::detectCores() - 1)
+
+normalize <- \(x) scale(x) |> as.vector()
 
 ###
-# Test with only Armenia - Azerbaijan conflict
+# Test with only Armenia - Azerbaijan conflict (conflict_id == 388)
 df <- readRDS("./data/merge.rds") |>
     group_by(conflict_id) |>
     arrange(year) |>
     mutate(duration = 1:n()) |>
-    ungroup()
-
-#sub.df <- filter(df, conflict_id %in% c(221, 223, 288, 227, 388) | (conflict_id == 401 & year <= 2007)) |>
-#    arrange(conflict_id, year)
-sub.df <- filter(df, conflict_id == 388) |>
+    ungroup() |>
     arrange(conflict_id, year)
 
 # Time-varying covariates affecting transition probabilities.
-X <- select(sub.df, ceasefire, tiv, duration) |>
-    mutate(duration = scale(duration) |> as.vector(),
-           duration_sq = scale(duration^2) |> as.vector(),
-           duration_cubed = scale(duration^3) |> as.vector(),
-           tiv = scale(tiv) |> as.vector())
-
-W <- select(sub.df, v2x_polyarchy, e_pop, e_gdppc) |>
-    mutate(v2x_polyarchy = scale(v2x_polyarchy) |> as.vector(),
-           e_pop = log(e_pop) |> scale() |> as.vector(),
-           e_gdppc = log(e_gdppc) |> scale() |> as.vector())
+X <- select(sub.df, ceasefire, tiv, v2x_polyarchy, e_gdppc, duration) |>
+    mutate(duration = log(duration) |> normalize(),
+           tiv = normalize(tiv),
+           e_gdppc = log(e_gdppc) |> normalize(),
+           v2x_polyarchy = normalize(v2x_polyarchy))
 
 conflicts <- mutate(sub.df, row = row_number()) |>
     group_by(conflict_id) |>
     summarise(start = first(row),
-              length = n())
+              end = last(row))
 
 data <- list(N = nrow(sub.df),
              K = 2,
              D = ncol(X),
-             M = ncol(W),
              n_conflicts = n_distinct(sub.df$conflict_id),
-             conflict_lens = conflicts$length,
              conflict_starts = conflicts$start,
-             W = data.matrix(W),
+             conflict_ends = conflicts$end,
              X = data.matrix(X),
              y = sub.df$brd)
 str(data)
 stopifnot(!anyNA(data))
 
 mod <- cmdstan_model("hmm.stan")
-fit <- mod$sample(data = data, chains = 4)
+fit <- mod$sample(data = data, chains = 4, adapt_delta = 0.95, max_treedepth = 12)
