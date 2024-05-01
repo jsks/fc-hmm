7f521706aa5e4ce743007e4093877d18c8c2b36b
Joshua Krusell <js.shirin@gmail.com>
2024-05-01 11:18:18 +0200

    Log priors for currently running model

--- scripts/hmm.sh
+++ scripts/hmm.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #
 #SBATCH -J hmm
-#SBATCH -t 04:00:00
+#SBATCH -t 06:00:00
 #SBATCH -n 4
 #SBATCH --mail-type=ALL
 
-echo "Starting HMM job at $(date)"
+printf "Starting HMM job at $(date)\n"
+printf "Model priors:\n"
+
+# This is a hack, but that's the tradeoff from using a minimal base
+# image without coreutils... Strange that `apptainer` doesn't have
+# a better method for extracting files.
+apptainer sif dump 4 hmm.sif > files.squashfs
+unsquashfs -q -d data files.squashfs -e hmm.json
+jq -c '{mu_location},{mu_scale},{sigma_scale},{tau_scale},{pi_alpha}' data/hmm.json
 
 apptainer run --bind $PWD:/data hmm.sif num_chains=4 num_threads=4 \
           output sig_figs=4 file=/data/output.csv
